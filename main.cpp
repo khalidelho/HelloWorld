99ad9c8a040a297ca3da3512371503decdea069c
Yusuf Pisan <pisan@uw.edu>
2018-05-25 13:06:37 -0700

    start

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    int i = 10;
+    assert(3 == 3);
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Travis here I come!" << std::endl;
+    return 0;
+}
